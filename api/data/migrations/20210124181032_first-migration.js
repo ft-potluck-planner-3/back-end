c7f65b9a34717bb4fba6c9035036c66329efafa7
Allison Homem <mrs.allisonhomem@outlook.com>
2021-10-21 13:30:50 -0500

    created potlucks table schema and seed data

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -13,6 +13,7 @@ exports.up = async (knex) => {
     .createTable('guests', (guests) => {
       guests.increments('guest_id')
       guests.string('guest_name')
+      guests.integer('user_id').unsigned().references('user_id').inTable('users').onUpdate('RESTRICT').onDelete('RESTRICT')
     })
     .createTable('potlucks', (potlucks) => {
       potlucks.increments('potluck_id')
@@ -20,15 +21,15 @@ exports.up = async (knex) => {
       potlucks.string('date').notNullable()
       potlucks.string('time').notNullable()
       potlucks.string('location').notNullable()
-      potlucks.integer('organizer').unsigned().notNullable().references('user_id').inTable('users').onUpdate('RESTRICT').onDelete('RESTRICT')
+      potlucks.integer('user_id').unsigned().notNullable().references('user_id').inTable('users').onUpdate('RESTRICT').onDelete('RESTRICT')
     })
     .createTable('potluck_foods', (potluck_foods) => {
-      potluck_foods.increments('potluck_foods_id')
+      potluck_foods.increments('potluck_food_id')
       potluck_foods.integer('potluck_id').unsigned().notNullable().references('potluck_id').inTable('potlucks').onUpdate('RESTRICT').onDelete('RESTRICT')
       potluck_foods.integer('food_id').unsigned().notNullable().references('food_id').inTable('foods').onUpdate('RESTRICT').onDelete('RESTRICT')
     })
     .createTable('potluck_guests', (potluck_guests) => {
-      potluck_guests.increments('potluck_guests_id')
+      potluck_guests.increments('potluck_guest_id')
       potluck_guests.integer('potluck_id').unsigned().notNullable().references('potluck_id').inTable('potlucks').onUpdate('RESTRICT').onDelete('RESTRICT')
       potluck_guests.integer('guest_id').unsigned().notNullable().references('guest_id').inTable('guests').onUpdate('RESTRICT').onDelete('RESTRICT')
     })
